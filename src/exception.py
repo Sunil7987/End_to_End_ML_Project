573eb2d8c8f25a2166f78a0c09977508a2750f50
sunil7987 <sunilsrajput156@gmail.com>
2024-06-24 23:46:08 +0530

    EDA and Modal trainin

--- src/exception.py
+++ src/exception.py
@@ -1,8 +1,6 @@
 import sys
-import logging
+from src.logger import logging
 
-print('1111')
-logging.basicConfig(level=logging.INFO)
 
 def error_message_details(error,error_detail:sys):
     _,_, exc_tb = error_detail.exc_info()
